0349148193bc5e156f432df218f48f800f690034
Rachel Hendricks <rachel.hendricks2@gmail.com>
2015-01-23 22:18:37 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
 $('.calc-button').on('click', function() {
 	var mlMin = $('.ml-min').val();
 	var rad = $('.rad').val();
 
-	var result = (mlMin * 60) / (Math.PI * rad * rad);
+	var result = (mlMin * 60) / (Math.PI * ((rad/2) * (rad/2));
 
 	$('.result').val(result.toFixed(3)); 
 });
@@ -12,7 +11,7 @@ $('.calc-button2').on('click', function() {
 	var cmHr = $('.cm-hr').val();
 	var rad2 = $('.rad2').val();
 
-	var result = (cmHr * Math.PI * rad2 * rad2) / (60);
+	var result = ((cmHr * Math.PI * ((rad2/2) * (rad2/2))) * (1/60);
 
 	$('.result2').val(result.toFixed(3)); 
 });
